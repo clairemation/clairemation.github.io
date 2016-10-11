9031aeab7e5d3aa557456ca8fa38ca241603e14a
Claire Samuels <clairemation@gmail.com>
2016-10-11 18:39:56 -0400

    Touch tests

--- projects/tpv2/view/touchinput.js
+++ projects/tpv2/view/touchinput.js
@@ -1,4 +1,4 @@
 document.body.addEventListener('touchstart', function(e){
     console.log(e);
-    document.getElementById('results').innerHTML = parseInt(e.changedTouches[0].clientX);
+    document.getElementById('results').innerHTML = "asdfasdf";
 });
