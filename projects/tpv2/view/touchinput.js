771209281c7d7abcaf0a85239bccd3dfb44d43ad
Claire Samuels <clairemation@gmail.com>
2016-10-11 19:01:02 -0400

    Tap test working

--- projects/tpv2/view/touchinput.js
+++ projects/tpv2/view/touchinput.js
@@ -1,4 +1,22 @@
-document.body.addEventListener('touchstart', function(e){
-    console.log(e);
-    document.getElementById('results').innerHTML = "asdfasdf";
+var ctx = document.getElementById('touchUI').getContext('2d');
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, 2000, 1000);
+
+window.addEventListener('touchstart', function(e){
+  e.preventDefault();
+  console.log(e.changedTouches);
+  var touch = e.changedTouches[0];
+  ctx.beginPath();
+  ctx.arc(touch.clientX, touch.clientY, 100, 0, 2*Math.PI, false);
+  ctx.closePath();
+  ctx.fillStyle = 'white';
+  ctx.fill();
 });
+
+window.addEventListener('touchend', function(e){
+  e.preventDefault();
+  var touch = e.changedTouches[0];
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 2000, 1000);
+})
